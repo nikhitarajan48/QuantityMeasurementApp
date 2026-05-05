736cbf6cbe03d46e6a20b338866d8f895e38490b
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-05-06 00:45:12 +0530

    UC2: Feet and Inches equality implemented

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -33,17 +33,45 @@ public class QuantityMeasurementApp {
         }
     }
 
+    // UC2: Inches Measurement Equality
+    static class Inch {
+        private final double value;
+
+        public Inch(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null || this.getClass() != obj.getClass())
+                return false;
+
+            Inch other = (Inch) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
     // Main method to test equality
     public static void main(String[] args) {
 
-        // Creating two Feet objects
+        // UC1: Feet Equality
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
+        System.out.println("Feet Equal (" + f1.equals(f2) + ")");
 
-        // Checking equality
-        boolean result = f1.equals(f2);
+        // UC2: Inch Equality (same)
+        Inch i1 = new Inch(1.0);
+        Inch i2 = new Inch(1.0);
+        System.out.println("Inch Equal (" + i1.equals(i2) + ")");
 
-        // Display result
-        System.out.println("Equal (" + result + ")");
+        // UC2: Inch Equality (different)
+        Inch i3 = new Inch(1.0);
+        Inch i4 = new Inch(2.0);
+        System.out.println("Inch Equal (" + i3.equals(i4) + ")");
+
     }
 }
